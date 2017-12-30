a72121dbee4b17964c978aed0c376b9a55c7c1d2
Jason Etcovitch <jasonetco@gmail.com>
2017-12-30 17:08:48 -0500

    Prevent Circle operation in non-PRs

--- tests/index.test.js
+++ tests/index.test.js
@@ -21,72 +21,100 @@ describe('ci-reporter', () => {
     ciReporter(robot)
   })
 
-  it('works with Travis CI', async () => {
-    const log = readFile(path.join('travis', 'log.txt'))
+  describe('Travis CI', () => {
+    it('creates the correct comment', async () => {
+      const log = readFile(path.join('travis', 'log.txt'))
 
-    nock('https://api.travis-ci.org')
-      .get('/builds/123').reply(200, JSON.stringify({
-        build: { pull_request_number: 1 },
-        jobs: [{ id: 1234, number: 1, state: 'failed' }]
-      }))
-      .get('/jobs/1234/log').reply(200, log)
+      nock('https://api.travis-ci.org')
+        .get('/builds/123').reply(200, JSON.stringify({
+          build: { pull_request_number: 1 },
+          jobs: [{ id: 1234, number: 1, state: 'failed' }]
+        }))
+        .get('/jobs/1234/log').reply(200, log)
 
-    const event = {
-      event: 'status',
-      payload: {
-        target_url: 'https://travis-ci.org/JasonEtco/public-test/builds/123?utm_source=github_status&utm_medium=notification',
-        context: 'continuous-integration/travis-ci/pr',
-        state: 'failure',
-        repository: {
-          name: 'public-test',
-          owner: { login: 'JasonEtco' }
-        },
-        installation: { id: 123 }
+      const event = {
+        event: 'status',
+        payload: {
+          target_url: 'https://travis-ci.org/JasonEtco/public-test/builds/123?utm_source=github_status&utm_medium=notification',
+          context: 'continuous-integration/travis-ci/pr',
+          state: 'failure',
+          repository: {
+            name: 'public-test',
+            owner: { login: 'JasonEtco' }
+          },
+          installation: { id: 123 }
+        }
       }
-    }
 
-    await robot.receive(event)
-    const args = github.issues.createComment.mock.calls[0]
+      await robot.receive(event)
+      const args = github.issues.createComment.mock.calls[0]
 
-    expect(args[0].body).toMatchSnapshot()
-    expect(args[0].number).toBe(1)
-    expect(args[0].owner).toBe('JasonEtco')
-    expect(args[0].repo).toBe('public-test')
+      expect(args[0].body).toMatchSnapshot()
+      expect(args[0].number).toBe(1)
+      expect(args[0].owner).toBe('JasonEtco')
+      expect(args[0].repo).toBe('public-test')
 
-    expect(github.issues.createComment).toHaveBeenCalledTimes(1)
+      expect(github.issues.createComment).toHaveBeenCalledTimes(1)
+    })
   })
 
-  it('works with Circle CI', async () => {
+  describe('Circle CI', () => {
     const build = readFile(path.join('circle', 'build.json'))
     const output = readFile(path.join('circle', 'output.json'))
+    const commit = readFile(path.join('circle', 'commit.json'))
 
-    nock('https://circleci.com')
-      .get('/api/v1.1/project/github/JasonEtco/todo/5').reply(200, build)
-      .get('/fake-output-url').reply(200, output)
+    it('creates the correct comment', async () => {
+      nock('https://circleci.com')
+        .get('/api/v1.1/project/github/JasonEtco/todo/5').reply(200, build)
+        .get('/fake-output-url').reply(200, output)
 
-    const event = {
-      event: 'status',
-      payload: {
-        target_url: 'https://circleci.com/gh/JasonEtco/todo/5?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link',
-        context: 'ci/circleci',
-        state: 'failure',
-        repository: {
-          name: 'todo',
-          owner: { login: 'JasonEtco' }
-        },
-        installation: { id: 123 }
+      const event = {
+        event: 'status',
+        payload: {
+          target_url: 'https://circleci.com/gh/JasonEtco/todo/5?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link',
+          context: 'ci/circleci',
+          state: 'failure',
+          repository: {
+            name: 'todo',
+            owner: { login: 'JasonEtco' }
+          },
+          installation: { id: 123 }
+        }
       }
-    }
 
-    await robot.receive(event)
-    const args = github.issues.createComment.mock.calls[0]
+      await robot.receive(event)
+      const args = github.issues.createComment.mock.calls[0]
 
-    expect(args[0].body).toMatchSnapshot()
-    expect(args[0].number).toBe(1)
-    expect(args[0].owner).toBe('JasonEtco')
-    expect(args[0].repo).toBe('todo')
+      expect(args[0].body).toMatchSnapshot()
+      expect(args[0].number).toBe(1)
+      expect(args[0].owner).toBe('JasonEtco')
+      expect(args[0].repo).toBe('todo')
 
-    expect(github.issues.createComment).toHaveBeenCalledTimes(1)
+      expect(github.issues.createComment).toHaveBeenCalledTimes(1)
+    })
+
+    it('does not create a comment if the status is not in a PR', async () => {
+      nock('https://circleci.com')
+        .get('/api/v1.1/project/github/JasonEtco/todo/5').reply(200, commit)
+        .get('/fake-output-url').reply(200, output)
+
+      const event = {
+        event: 'status',
+        payload: {
+          target_url: 'https://circleci.com/gh/JasonEtco/todo/5?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link',
+          context: 'ci/circleci',
+          state: 'failure',
+          repository: {
+            name: 'todo',
+            owner: { login: 'JasonEtco' }
+          },
+          installation: { id: 123 }
+        }
+      }
+
+      await robot.receive(event)
+      expect(github.issues.createComment).not.toHaveBeenCalled()
+    })
   })
 
   it('does nothing if the status context is not accounted for', async () => {
